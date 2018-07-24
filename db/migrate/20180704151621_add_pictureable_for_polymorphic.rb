b70ce8a97fdbfbe9b026e038fe34cf6e41dfdbb3
Diep The Toan <diepthetoan1@gmail.com>
2018-07-24 22:42:29 +0700

    Delete creating pictureable migration

--- db/migrate/20180704151621_add_pictureable_for_polymorphic.rb
+++
@@ -1,7 +0,0 @@
-class AddPictureableForPolymorphic < ActiveRecord::Migration[5.2]
-  def change
-    add_column :pictures, :pictureable_id, :integer
-    add_column :pictures, :pictureable_type, :string
-    add_index :pictures, [ :pictureable_id, :pictureable_type ]
-  end
-end
