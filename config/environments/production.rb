04e7e7eb4c99ef78e87ac94d783aae6dbda50aac
Diep The Toan <diepthetoan1@gmail.com>
2018-07-31 09:35:31 +0700

    Edit production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,7 +98,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.default :charset => "utf-8"
 
@@ -108,10 +108,10 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'heroku.com',
+    domain:               'intense-citadel-58248.herokuapp.com',
     user_name:            'diepthetoan1@gmail.com',
     password:             ENV["GMAIL_SMTP_PASSWORD"],
-    authentication:       :plain,
+    authentication:       'plain',
     enable_starttls_auto:  true 
   }
 
