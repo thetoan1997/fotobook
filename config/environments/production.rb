13e37b56ac3ceffdb2effc869497f3a1662bc4ff
Diep The Toan <diepthetoan1@gmail.com>
2018-07-31 08:49:40 +0700

    Edit database.yml for sending mail by heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,7 +98,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
   
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
