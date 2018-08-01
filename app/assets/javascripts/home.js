d159912be6d239c95a0112d304f23e5537e3fe95
Diep The Toan <diepthetoan1@gmail.com>
2018-08-01 11:09:43 +0700

    Edit view of login, signup, edit pagination at home page

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -12,12 +12,36 @@ $(document).on('turbolinks:load', function() {
         }
     });
 
-    $.urlParam = function(name){
-        var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
-        return results[1] || 0;
-    }
-    if($.urlParam('page_album_feeds') != null){
-       $('.content-post-photos').removeClass('in active');
-       $('.content-post-albums').addClass('in active');
-    }
+    // $.urlParam = function(name){
+    //     var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+    //     return results[1] || 0;
+    // }
+    // if($.urlParam('page_album_feeds') != null){
+    //    $('.content-post-photos').removeClass('in active');
+    //    $('.content-post-albums').addClass('in active');
+    // }
+    // else if($.urlParam('page_photo_discover') != null){
+    //     $('.main-content-feeds').removeClass('active in');
+    //     $('.main-content-discover').addClass('active in');
+    // }
+    $('#myTab a').click(function(e) {
+        e.preventDefault();
+        $(this).tab('show');
+      });
+      
+      // store the currently selected tab in the hash value
+      $("ul.nav-tabs > li > a").on("shown.bs.tab", function(e) {
+        var id = $(e.target).attr("href").substr(1);
+        window.location.hash = id;
+      });
+      
+      // on load of the page: switch to the currently selected tab
+      var hash = window.location.hash;
+      $('#myTab a[href="' + hash + '"]').tab('show');
+
+      $('#option-photo-discover span').click(function(){
+          alert("yes");
+            $('.main-content-discover').addClass('active in');
+            $('.main-content-feeds').removeClass('active in');
+      });
 });
